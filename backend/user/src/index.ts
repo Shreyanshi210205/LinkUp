40623b2db9d2cb3407ebf6c02adea1054fc22f33
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2026-03-19 13:32:11 +0530

    feat: integrate Redis client and connection handling
    - Added Redis client setup using the `redis` package.
    - Implemented connection logic for Redis with error handling.
    - Updated environment configuration to include Redis URL.
    - Modified package.json and package-lock.json to include Redis dependencies.
    - Updated TypeScript definitions and generated declaration maps.

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,9 +1,35 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js';
+import {createClient} from 'redis'
 
 dotenv.config()
 connectDb()
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error:", err);
+});
+
+async function connectRedis() {
+  try {
+    console.log("Connecting to:", process.env.REDIS_URL);
+    await redisClient.connect();
+    console.log("Redis connected successfully 🚀");
+  } catch (error) {
+    console.error("Redis connection failed:", error);
+  }
+}
+console.log(process.env.REDIS_URL);
+connectRedis();
+
 const app=express();
 const port=process.env.PORT || 5000
 app.listen(port,()=>{
