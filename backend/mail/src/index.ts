dbc2a8e71fa3adb7d19e8c1d8f182c49c19a19a3
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2026-03-27 00:52:53 +0530

    feat(mail): update package.json and add OTP email consumer
    - Updated package.json to include nodemon and ts-node-dev for improved development experience.
    - Refactored index.ts to streamline server initialization and logging.
    - Added consumer.ts to handle sending OTP emails using RabbitMQ and Nodemailer.

--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv"
-
-dotenv.config();
-
 const app=express();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
+
+dotenv.config();
+const PORT=process.env.PORT || 5001;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
 });
