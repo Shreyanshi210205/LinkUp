7ca71d146f6b13daaed41c8d7cadca05994aa57c
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2026-03-30 14:49:13 +0530

    feat(user): implement OTP verification and user creation logic

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { publishToQueue } from "../config/rabbitmq.js";
 import TryCatch from "../config/TryCatch.js";
 import { redisClient } from "../index.js";
+import { User } from "../model/User.js";
 
 export const loginUser= TryCatch(async(req,res)=>{
     const {email}=req.body
@@ -27,4 +28,31 @@ await publishToQueue("send-otp",message)
 res.status(200).json({
     message:"OTP sent to your email"
 })
+})
+
+export const verify=TryCatch(async(req,res)=>{
+    const {email,otp:enteredOtp}=req.body
+    if(!email || !enteredOtp) {
+        res.status(400).json({
+            message:"Email and otp required"
+        })
+        return;
+    }
+
+    const otpKey=`otp:${email}`
+    const storedOtp=await redisClient.get(otpKey)
+    if(!storedOtp || storedOtp!==enteredOtp) {
+        res.status(400).json({
+        message:"Invalid or expired otp"
+    })
+    return
+}
+await redisClient.del(otpKey)
+let user =await User.findOne({email});
+if(!user){
+    const name=email.slice(0,8)
+    user=await User.create({name,email})
+}
+
+
 })
