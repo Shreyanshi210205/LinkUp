eb539db694fdd581c7e7dd39198d793d0cc7d1cf
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2026-04-01 01:12:35 +0530

    feat(user): add update name functionality for user profile

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -68,3 +68,22 @@ export const myProfile=TryCatch(async(req:AuthenticatedRequest,res)=>{
     const user=req.user
     res.json(user)
 })
+
+export const updateName=TryCatch(async(req:AuthenticatedRequest,res)=>{
+    const user=await User.findById(req.user?._id)
+    if(!user){
+        res.status(404).json({
+            message:"Please login"
+        })
+        return
+    }
+    user.name=req.body.name
+    await user.save()
+
+    const token=generateToken(user)
+    res.json({
+        message:"User updated",
+        user,
+        token
+    })
+})
