059f74c9a31eac8a2cb791c2af738bd71959ae39
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2026-04-03 15:55:37 +0530

    feat(chat): add getAllChats endpoint and create Messages model

--- backend/chat/src/controllers/chat.ts
+++ backend/chat/src/controllers/chat.ts
@@ -29,3 +29,20 @@ export const createNewChat=TryCatch(async(req:AuthenticatedRequest,res)=>{
         chatId:newChat._id
     })
 })
+
+export const getAllChats=TryCatch(async(req:AuthenticatedRequest,res)=>{
+    const userId=req.user?._id
+    if(!userId){
+        res.status(400).json({
+            message:"User id missing"
+        })
+        return
+    }
+    const chats=await Chat.find({users:userId}).sort({updatedAt:-1});
+    const chatWithUserData=await Promise.all(
+        chats.map(async(chat)=>{
+            const otherUserId=chat.users.find(id=>id!==userId)
+            
+        })
+    )
+})
