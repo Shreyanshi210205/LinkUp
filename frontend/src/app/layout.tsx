aec901b19e3ef232217856b3c7344cefb60e7f58
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2026-06-02 03:27:51 +0530

    feat(socket): integrate Socket.IO for real-time communication and add SocketContext for managing online users

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AppProvider } from "../context/AppContext";
+import { SocketProvider } from "../context/SocketContext";
 
 
 
@@ -20,7 +21,8 @@ export default function RootLayout({
     >
       <body >
         <AppProvider>
-        {children}
+          <SocketProvider>{children}</SocketProvider>
+        
         </AppProvider></body>
     </html>
   );
---
+++ frontend/src/context/SocketContext.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { createContext, ReactNode, useContext, useEffect, useMemo, useState } from "react"
+import { io, Socket } from "socket.io-client"
+import { chat_service, useAppData } from "./AppContext"
+
+interface SocketContextType{
+    socket:Socket|null
+    onlineUsers:string[]
+}
+
+const SocketContext=createContext<SocketContextType>({
+    socket:null,
+    onlineUsers:[]
+})
+
+interface ProviderProps{
+    children:ReactNode
+}
+
+export const SocketProvider=({children}:ProviderProps)=>{
+    const [socket,setSocket]=useState<Socket|null>(null)
+    const {user}=useAppData()
+    const [onlineUsers,setOnlineUsers]=useState<string[]>([])
+
+    useEffect(()=>{
+        if(!user?._id) return;
+        const newSocket=io(chat_service,{
+            query:{
+                userId:user._id
+            }
+        })
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setSocket(newSocket)
+
+        newSocket.on("getOnlineUser",(users:string[])=>{
+            setOnlineUsers(users)
+        })
+        return ()=>{
+            newSocket.disconnect()
+        }
+
+    },[user?._id])
+}
+
+export const SocketData=()=>{
+    return useContext(SocketContext)
+}
